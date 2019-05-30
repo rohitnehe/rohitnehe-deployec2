08fd8e57d04795423b16d5dd5f6a033e316511bd
Ajay Khadke <ajaykhadke@benchmarkitsolutions.com>
2019-05-30 20:17:54 +0530

    1st commit

---
+++ index.php
@@ -0,0 +1,25 @@
+
+<?php
+include_once 'connections.php';
+$sql = "SELECT * FROM student";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Mobile Number</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["id"]."</td><td>".$row["first_name"]."</td><td>".$row["last_name"]."</td><td>".$row["phone"]."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+?>
+<style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 15px;
+}
+</style>
