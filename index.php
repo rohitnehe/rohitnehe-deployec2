cd8359354151353a087b3f154a1bdeb1887a7012
Ajay Khadke <ajaykhadke@benchmarkitsolutions.com>
2019-06-10 18:53:08 +0530

    space removed2

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $sql = "SELECT * FROM student";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
 <<<<<<< HEAD
-    echo "<table><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Mobile Number</th></tr>";
+    echo "<table><tr><th>ID</th><th>FirstName</th><th>Last Name</th><th>Mobile Number</th></tr>";
 =======
     echo "<table><tr><th>ID</th><th>First Name2</th><th>Last Name1</th><th>phone Number</th></tr>";
 >>>>>>> 7db4a0a379e9e852f4cd92fe08238335fef46fce
