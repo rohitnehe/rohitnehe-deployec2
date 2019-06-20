6d071ba0c936a76a84c1bc0cf427f0ec1017b664
Ajay Khadke <ajaykhadke@benchmarkitsolutions.com>
2019-06-20 15:38:27 +0530

    pr

---
+++ connections.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "advdemo.chsnmzlx99md.us-east-1.rds.amazonaws.com";
+$username = "advdemo";
+$password = "1P51aZ3TTTettTem";
+$dbname = "advdemo";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+//echo "Connected successfully";
+?>
---
+++ index.php
@@ -0,0 +1,29 @@
+
+<?php
+include_once 'connections.php';
+$sql = "SELECT * FROM student";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+<<<<<<< HEAD
+    echo "<table><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Mobile Number</th></tr>";
+=======
+    echo "<table><tr><th>ID</th><th>First Name2</th><th>Last Name1</th><th>phone Number</th></tr>";
+>>>>>>> 7db4a0a379e9e852f4cd92fe08238335fef46fce
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["id"]."</td><td>".$row["first_name"]."</td><td>".$row["last_name"]."</td><td>".$row["mobile"]."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+?>
+<style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 15px;
+}
+</style>
